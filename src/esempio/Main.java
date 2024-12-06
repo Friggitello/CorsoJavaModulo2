25f8f20ca9d238f33716a0f68a223c8488e328b1
Friggitello <fggabrielito@gmail.com>
2024-12-06 12:58:30 +0100

    Aggiunta esempi di Classi e Oggetti

--- src/esempio/Main.java
+++ src/esempio/Main.java
@@ -1,9 +1,17 @@
 package esempio;
 
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
         Persona p1 = new Persona();
-        Persona p2 = new Persona();
+        Persona p2 = new Persona("Gabriel", "David");
+        p2.dataNascita = LocalDate.of(1999,11,15);
+        p1.nome = "Gabriel";
+        p1.cognome = "David";
         System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p2 == p1);
+        System.out.println(p1.equals(p2));
     }
 }
--- src/esempio/Persona.java
+++ src/esempio/Persona.java
@@ -4,6 +4,8 @@ import java.time.LocalDate;
 
 public class Persona {
 
+    public static Integer idCounter = 0;
+    public Integer id;
     public String nome;
     public String cognome;
     public LocalDate dataNascita;
@@ -13,20 +15,40 @@ public class Persona {
     //Se non definisco alcun costruttore viene aggiunto da java
     //il costruttore di default
 
-    public Persona(){}
+    public Persona() {
+        this.id = ++idCounter;
+    }
 
-    public Persona(String nome, String cognome){
+    public Persona(String nome, String cognome) {
+        this.id = ++idCounter;
         this.nome = nome;
         this.cognome = cognome;
     }
-    @Override
+
+    @Override //Metadati della classe
     public String toString() {
-        return "Persona{" +
-                "nome='" + nome + '\'' +
+        return "Persona {" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
                 ", cognome='" + cognome + '\'' +
                 ", dataNascita=" + dataNascita +
                 ", luogoNascita='" + luogoNascita + '\'' +
                 ", codiceFiscale='" + codiceFiscale + '\'' +
                 '}';
+    }
+
+    @Override //equals di base fa il == tra due oggetti,
+              // che di base darebbe la locazione di memoria
+              //Quindi si puo riprogrammare per definire
+              //quando sono uguali due oggetti in base ai propri valori
+    public boolean equals(Object obj) {
+        // la prima cosa da verificare se obj è una Persona
+        if (obj.getClass() != this.getClass()){
+            return false;
+        }
+        Persona persona = (Persona) obj;
+        return persona.nome.equals(this.nome) && persona.cognome.equals(this.cognome);
+        //In questo caso ho definito che sono uguali solo se hanno nome e cognome uguali
+        //e non si basa sugli altri attributi dell'oggetto
     }
 }
